abd99819d4a10314d1a4723c7e25249c71906a43
kjh1234 <kjh1234@gmail.com>
2020-07-08 11:22:23 +0900

    Fetch 기능 추가 - React, Svelte, Vue

--- svelte-ajax-router/src/pages/main/About.svelte
+++ svelte-ajax-router/src/pages/main/About.svelte
@@ -1,3 +1,10 @@
+
+
+<script>
+import Template from '../../../public/test.html'
+console.log(template)
+</script>
+
 <div>
   <h1>About</h1>
   <p>How am I?</p>
---
+++ svelte-dashboard/src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Router, Route } from 'yrv'
+	import Home from '../pages/main/Home.svelte'
+	import About from '../pages/main/About.svelte'
+	import NotFound from '../pages/exception/NotFound.svelte'
+</script>
+
+<Router>
+	<Route exact path="/" redirect="/#!" />
+	<Route exact path="#!" component={Home} />
+	<Route exact path="#!/about" component={About} />
+	<Route fallback component={NotFound} />
+</Router>
